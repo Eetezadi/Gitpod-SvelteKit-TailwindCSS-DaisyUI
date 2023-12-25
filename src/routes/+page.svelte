2b3aa81133cdeded1b89db70d5064999807c3b80
Sina Eetezadi <code@eetezadi.com>
2023-12-25 14:44:59 +0000

    Updated all packges

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>FBS-MVP</h1>
+<p>Lets start this journey with Svelte... :)</p>
