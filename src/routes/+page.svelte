c4d472e55c7893f853af66827672bb743c3212f9
Sina Eetezadi <code@eetezadi.com>
2023-12-25 15:30:35 +0000

    added tailwind and daysiUI. Working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
 <h1>FBS-MVP</h1>
 <p>Lets start this journey with Svelte... :)</p>
+
+<h1>Lets say Hi to DaysiUI...</h1>
+
+<div class="mockup-phone">
+	<div class="camera"></div>
+	<div class="display">
+		<div class="artboard artboard-demo phone-1">
+			Hi, I am DaysiUI and I brought 3 buttons:
+			<button class="btn btn-primary">One</button>
+			<button class="btn btn-secondary">Two</button>
+			<button class="btn btn-accent btn-outline">Three</button>
+		</div>
+	</div>
+</div>
